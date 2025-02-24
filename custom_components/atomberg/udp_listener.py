8e8f243b017eb1723fbbefa95471eb9b9c32737b
Shubham Das <dasshubham762@gmail.com>
2025-02-24 14:52:46 +0530

    udp_listener: Add support for proxy protocol

--- custom_components/atomberg/udp_listener.py
+++ custom_components/atomberg/udp_listener.py
@@ -19,17 +19,31 @@ class UDPListener(asyncio.DatagramProtocol):
         self._listener = None
         self._callback = None
 
+    def parse_datagram(self, data, addr) -> tuple[str, str]:
+        """Decode and parse the data."""
+        message: str = data.decode(errors="ignore")
+
+        if message.startswith("PROXY "):
+            parts = message.split()
+            if len(parts) >= 6 and parts[1] in ["TCP4"]:
+                proxy_ip: str = parts[2]
+                actual_message = " ".join(parts[6:])
+                return actual_message, proxy_ip
+
+        return message, addr[0]
+
     def datagram_received(self, data, addr):
         """Decode data when broadcast received."""
-        message = data.decode("utf-8")
-        _LOGGER.debug("Message received: %s", message)
+        message, ip_addr = self.parse_datagram(data, addr)
+        _LOGGER.debug("Message received %s from %s", message, ip_addr)
 
         # Try to hexdecode the message
+        msg_data = {"ip_address": ip_addr}
         try:
-            msg_data = bytes.fromhex(message)
-            msg_data = json.loads(msg_data)
+            msg_data_bytes = bytes.fromhex(message)
+            msg_data.update(json.loads(msg_data_bytes))
         except ValueError:
-            msg_data = {"device_id": message.split("_")[0]}
+            msg_data.update({"device_id": message.split("_")[0]})
 
         if self._callback:
             self._callback(msg_data)
